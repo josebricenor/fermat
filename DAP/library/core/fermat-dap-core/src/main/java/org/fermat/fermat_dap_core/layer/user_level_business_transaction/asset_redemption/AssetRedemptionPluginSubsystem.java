ebe03bd79f3d0e650e6b04c73e171edc6571acfd
lutor <lutor1106@gmail.com>
2016-04-19 15:22:40 -0430

    #6023

--- DAP/library/core/fermat-dap-core/src/main/java/org/fermat/fermat_dap_core/layer/user_level_business_transaction/asset_redemption/AssetRedemptionPluginSubsystem.java
+++ DAP/library/core/fermat-dap-core/src/main/java/org/fermat/fermat_dap_core/layer/user_level_business_transaction/asset_redemption/AssetRedemptionPluginSubsystem.java
@@ -5,6 +5,9 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_core_api.layer.all_definition.system.abstract_classes.AbstractPluginSubsystem;
 import com.bitdubai.fermat_core_api.layer.all_definition.system.exceptions.CantStartSubsystemException;
 
+import org.fermat.fermat_dap_plugin.layer.user_level_business_transaction.asset_redemption.developer.DeveloperBitDubai;
+
+
 /**
  * Created by Víctor A. Mars M. (marsvicam@gmail.com) on 12/04/16.
  */
@@ -20,7 +23,13 @@ public class AssetRedemptionPluginSubsystem extends AbstractPluginSubsystem {
     //PUBLIC METHODS
     @Override
     public void start() throws CantStartSubsystemException {
-
+        try {
+            registerDeveloper(new DeveloperBitDubai());
+            System.out.println("LFTL: ULBT ASSET REDEMPTION");
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            throw new CantStartSubsystemException(e, null, null);
+        }
     }
     //PRIVATE METHODS
 
