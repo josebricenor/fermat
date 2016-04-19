96adcb275c30ccd1313e612ff68b72007567ce79
lutor <lutor1106@gmail.com>
2016-04-19 15:12:12 -0430

    #6020

---
+++ DAP/plugin/user_level_business_transaction/fermat-dap-plugin-user-level-business-transaction-asset-transfer/src/main/java/org/fermat/fermat_dap_plugin/layer/user_level_business_transaction/asset_transfer/developer/version_1/structure/database/AssetTransferDatabaseConstants.java
@@ -0,0 +1,24 @@
+package org.fermat.fermat_dap_plugin.layer.user_level_business_transaction.asset_transfer.developer.version_1.structure.database;
+
+/**
+ * Created by Víctor A. Mars M. (marsvicam@gmail.com) on 9/02/16.
+ */
+public class AssetTransferDatabaseConstants {
+    //DATABASE
+    public static final String ASSET_TRANSFER_DATABASE = "asset_transfer_database";
+
+    //ASSET TRANSFER TABLE
+    public static final String ASSET_TRANSFER_TABLE = "asset_transfer";
+    public static final String ASSET_TRANSFER_ID_COLUMN_NAME = "id";
+
+    //EVENTS RECORDED TABLE
+    public static final String ASSET_TRANSFER_EVENTS_RECORDED_TABLE_NAME = "events_recorded";
+
+    public static final String ASSET_TRANSFER_EVENTS_RECORDED_ID_COLUMN_NAME = "event_id";
+    public static final String ASSET_TRANSFER_EVENTS_RECORDED_EVENT_COLUMN_NAME = "event";
+    public static final String ASSET_TRANSFER_EVENTS_RECORDED_SOURCE_COLUMN_NAME = "source";
+    public static final String ASSET_TRANSFER_EVENTS_RECORDED_STATUS_COLUMN_NAME = "status";
+    public static final String ASSET_TRANSFER_EVENTS_RECORDED_TIMESTAMP_COLUMN_NAME = "timestamp";
+
+    public static final String ASSET_TRANSFER_EVENTS_RECORDED_TABLE_FIRST_KEY_COLUMN = ASSET_TRANSFER_EVENTS_RECORDED_ID_COLUMN_NAME;
+}
