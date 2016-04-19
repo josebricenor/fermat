e58b4dc491ffbf946875e02d5d8dfae550321e46
Víctor A. Mars M <marsvicam@gmail.com>
2016-04-19 17:10:12 -0430

    REFACTOR: Create DefaultDAPEventListener to avoid code repeat since these classes can be generified.

--- DAP/library/api/fermat-dap-api/src/main/java/org/fermat/fermat_dap_api/layer/all_definition/listeners/DefaultDAPEventListener.java
+++ DAP/library/api/fermat-dap-api/src/main/java/org/fermat/fermat_dap_api/layer/all_definition/listeners/DefaultDAPEventListener.java
@@ -1,24 +1,51 @@
 package org.fermat.fermat_dap_api.layer.all_definition.listeners;
 
-import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEvent;
+import com.bitdubai.fermat_api.FermatException;
 import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventHandler;
 import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventListener;
 import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventMonitor;
+
 import org.fermat.fermat_dap_api.layer.all_definition.enums.EventType;
+import org.fermat.fermat_dap_api.layer.all_definition.events.AbstractDAPEvent;
 
 /**
- * Created by Nerio on 18/02/16.
+ * This class is the default event listener for mostly of DAP Events.
+ * <p/>
+ * Created by Víctor A. Mars M. (marsvicam@gmail.com) on 19/04/16.
  */
-public class ActorAssetNetworkServicePendingsNotificationEventListener implements FermatEventListener {
+public class DefaultDAPEventListener<Z extends AbstractDAPEvent> implements FermatEventListener<Z, EventType> {
 
-    FermatEventMonitor fermatEventMonitor;
+    //VARIABLE DECLARATION
+
+    private FermatEventMonitor fermatEventMonitor;
     private EventType eventType;
-    private FermatEventHandler fermatEventHandler;
+    private FermatEventHandler<Z> fermatEventHandler;
 
-    public ActorAssetNetworkServicePendingsNotificationEventListener(EventType eventType, FermatEventMonitor fermatEventMonitor) {
+    //CONSTRUCTORS
+    public DefaultDAPEventListener(EventType eventType, FermatEventMonitor fermatEventMonitor) {
         this.eventType = eventType;
         this.fermatEventMonitor = fermatEventMonitor;
     }
+
+
+    //PUBLIC METHODS
+
+    /**
+     * Throw the method <code>raiseEvent</code> you can raise the event to be listened.
+     *
+     * @param fermatEvent an instance of fermat event to be listened.
+     */
+    @Override
+    public void raiseEvent(Z fermatEvent) {
+        try {
+            fermatEventHandler.handleEvent(fermatEvent);
+        } catch (FermatException e) {
+            fermatEventMonitor.handleEventException(e, fermatEvent);
+        }
+    }
+    //PRIVATE METHODS
+
+    //GETTER AND SETTERS
 
     /**
      * Throw the method <code>getEventType</code> you can get the information of the event type.
@@ -36,7 +63,7 @@ public class ActorAssetNetworkServicePendingsNotificationEventListener implement
      * @param fermatEventHandler handler for the event listener.
      */
     @Override
-    public void setEventHandler(FermatEventHandler fermatEventHandler) {
+    public void setEventHandler(FermatEventHandler<Z> fermatEventHandler) {
         this.fermatEventHandler = fermatEventHandler;
     }
 
@@ -47,20 +74,8 @@ public class ActorAssetNetworkServicePendingsNotificationEventListener implement
      */
     @Override
     public FermatEventHandler getEventHandler() {
-        return this.fermatEventHandler;
+        return fermatEventHandler;
     }
 
-    /**
-     * Throw the method <code>raiseEvent</code> you can raise the event to be listened.
-     *
-     * @param fermatEvent an instance of fermat event to be listened.
-     */
-    @Override
-    public void raiseEvent(FermatEvent fermatEvent) {
-        try {
-            this.fermatEventHandler.handleEvent(fermatEvent);
-        } catch (Exception exception) {
-            fermatEventMonitor.handleEventException(exception, fermatEvent);
-        }
-    }
+    //INNER CLASSES
 }
